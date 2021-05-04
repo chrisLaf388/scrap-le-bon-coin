6077d4263f772268c26fe65f3815bc9711cb69b4
LAFENETRE Christophe <C.LAFENETRE@mars-marketing.fr>
2021-05-04 16:25:49 +0200

    scarp 1 page  ok

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pandas as pd
 import time
 from datetime import datetime
 
+tels= []
+
 
 # Ouverture de la page Chrome, Démarrage du robot
 driver = webdriver.Chrome('chromedriver.exe')
@@ -21,28 +23,60 @@ time.sleep(10)
 driver.find_element_by_xpath('//*[@id="didomi-notice-agree-button"]').click()
 time.sleep(5)
 
-#click de page suivante
-driver.find_element(By.CSS_SELECTOR, ".\\_2WNWW:nth-child(5) .sc-bdvvaa").click()
-time.sleep(5)
 
 #affiche numero de page
 numeroPageActuel = driver.find_element(By.CSS_SELECTOR, ".\\_21IEe").text
 
-for i in range (10,20):
-    try:
-    #click sur la card de l'annonce
-        driver.find_element(By.CSS_SELECTOR, '.styles_adCard__2YFTi:nth-child('+str(i)+') .AdCardTitle-e546g7-0').click()
+print(numeroPageActuel)
+while numeroPageActuel != driver.find_element(By.CSS_SELECTOR, ".\\_21IEe").text:
+    for i in range (1,42):
+        try:
+            #click sur la card de l'annonce
+            driver.find_element(By.CSS_SELECTOR, '.styles_adCard__2YFTi:nth-child('+str(i)+')').click()
+            time.sleep(3)
+
+            try:
+                #Verifie si le bouton numéro est présent
+                if (driver.find_element_by_xpath('//*[@id="aside"]/div[1]/section/div/div[3]/div[2]/div[1]/button').text == "Voir le numéro"):
+                    driver.find_element_by_xpath('//*[@id="aside"]/div[1]/section/div/div[3]/div[2]/div[1]/button').click()
+                    time.sleep(1)
+                    print(driver.find_element_by_xpath('//*[@id="aside"]/div[1]/section/div/div[3]/div[2]/div[1]/a').text)
+                    tel = driver.find_element_by_xpath('//*[@id="aside"]/div[1]/section/div/div[3]/div[2]/div[1]/a').text
+
+                    telExistant = False
+                    for j in tels:
+                        if tel == j :
+                            telExistant=True
+
+                    if telExistant != True :
+                        tels.append(tel)
+
+                else:
+                    print("pas de num")
+            except:
+                driver.back()
+                time.sleep(5)
+
+
+            driver.back()
+            time.sleep(5)
+        except:
+            pass
+
+        # affiche numero de page
+        numeroPagePrecedent = driver.find_element(By.CSS_SELECTOR, ".\\_21IEe").text
+
+        # click de page suivante
+        driver.find_element(By.CSS_SELECTOR, ".\\_2WNWW:nth-child(5) .sc-bdvvaa").click()
         time.sleep(5)
 
-        #Clique sur affiche le numéro
-        driver.find_element(By.CSS_SELECTOR, '.tiOck > .\_2qvLx').click()
-        time.sleep(1)
-        #Affiche le numéro
-        print(driver.find_element(By.CSS_SELECTOR, '.tiOck > .\_2qvLx').text)
-    except:
-        pass
+        # affiche numero de page
+        numeroPageActuel = driver.find_element(By.CSS_SELECTOR, ".\\_21IEe").text
 
-    driver.back()
-    time.sleep(5)
+
+
+
+
+
 
 
